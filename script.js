5169d973cf7597941c85393b632d914818cf7bdf
habyaad <adeomotoshan@gmail.com>
2023-10-19 22:10:14 +0100

    -add backspace function -modify appendToDisplay function to handle special cases -initiate calculator to 0

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
-let currentInput = '';
+let currentInput = '0';
+document.getElementById('display').value = currentInput;
+let operators = ["+", "-", "/", "*", "%"]
 
+function lastCurrentInputSymbol() {
+    return currentInput[currentInput.length-1];
+}
+function containsAnyOperator(string) {
+    for (var i = 0; i < operators.length-1; i++) {
+      if (string.includes(operators[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
 function appendToDisplay(value) {
-    currentInput += value;
-    document.getElementById('display').value += value;
+    if(currentInput == "0"){
+        currentInput = value;
+        document.getElementById('display').value = currentInput;
+    }else if(operators.includes(lastCurrentInputSymbol()) && operators.includes(value)){
+        currentInput = currentInput.substring(0,currentInput.length-1).concat(value);
+        document.getElementById('display').value = currentInput;
+    }else if(value == "."){
+        if(lastCurrentInputSymbol() == "."){
+
+        }else if(currentInput.includes(".")){
+            let lastIndex = currentInput.lastIndexOf(".")
+            if(containsAnyOperator(currentInput.substring(lastIndex, currentInput.length))){
+                currentInput += value;
+                document.getElementById('display').value += value;
+            }else{
+
+            }
+        }else{
+            currentInput += value;
+            document.getElementById('display').value += value;
+        }
+    }
+    else{
+        currentInput += value;
+        document.getElementById('display').value += value;
+    }
 }
 
 function clearDisplay() {
-    currentInput = '';
-    document.getElementById('display').value = '';
+    currentInput = '0';
+    document.getElementById('display').value = currentInput;
 }
 
 function calculateResult() {
@@ -48,5 +85,16 @@ function calculateSquare() {
     } catch (error) {
         document.getElementById('display').value = 'Error';
         currentInput = '';
+    }
+}
+function eraseCurrentInput() {
+    if(currentInput == "0"){
+      
+    }else if(currentInput.length == 1){
+        currentInput = "0";
+        document.getElementById('display').value = currentInput;
+    }else{
+        currentInput = currentInput.substring(0,currentInput.length-1);
+        document.getElementById('display').value = currentInput;
     }
 }
